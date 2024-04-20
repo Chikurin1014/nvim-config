b7bb63a5ceae6f086791c91979eb83d8731eda13
Chikurin1014 <chikurin1014@gmail.com>
2024-04-20 22:01:40 +0900

    change some settings

--- files/lua/plugins/sidebar.lua
+++ files/lua/plugins/sidebar.lua
@@ -3,7 +3,7 @@ return {
         'sidebar-nvim/sidebar.nvim',
         config = function()
             require('sidebar-nvim').setup({
-                open = true,
+                open = false,
                 side = 'right',
                 initial_width = 25,
                 sections = { 'git', 'containers', 'diagnostics' }
