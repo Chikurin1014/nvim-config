5f3fabbdd60ad33b2ed384fca9505da43614b567
Chikurin1014 <chikurin1014@gmail.com>
2024-06-06 00:28:31 +0900

    fix toggleterm settings

--- files/lua/plugins/toggleterm.lua
+++ files/lua/plugins/toggleterm.lua
@@ -6,7 +6,12 @@ return {
         config = function()
             local map = vim.api.nvim_set_keymap
             local opts = {}
-            -- map('n', '<leader>w', '<cmd>ToggleTerm<cr>', opts)
+            local Terminal = require('toggleterm.terminal').Terminal
+            local lazygit = Terminal:new({ cmd = 'lazygit', hidden = true, direction = 'float' })
+            function _lazygit_toggle()
+                lazygit:toggle()
+            end
+            map('n', '<leader>t', '<CMD>lua _lazygit_toggle()<CR>', opts)
         end
     }
 }
