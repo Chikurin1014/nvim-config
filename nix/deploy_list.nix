ac259c5e99ae1240b49e0e867da9b429206f85ad
Chikurin1014 <chikurin1014@gmail.com>
2025-06-16 00:57:27 +0900

    :sparkles: add nix expression to deploy files

---
+++ nix/deploy_list.nix
@@ -0,0 +1,26 @@
+{ src }:
+
+{
+    ".config/nvim/init.lua".source               = src + "/files/init.lua";
+    ".config/nvim/lua/base.lua".source           = src + "/files/lua/base.lua";
+    ".config/nvim/lua/lazy_nvim.lua".source      = src + "/files/lua/lazy_nvim.lua";
+    ".config/plugins/alpha.lua".source           = src + "/files/plugins/alpha.lua";
+    ".config/plugins/autopairs.lua".source       = src + "/files/plugins/autopairs.lua";
+    ".config/plugins/bufferline.lua".source      = src + "/files/plugins/bufferline.lua";
+    ".config/plugins/buffersize.lua".source      = src + "/files/plugins/buffersize.lua";
+    ".config/plugins/comment.lua".source         = src + "/files/plugins/comment.lua";
+    ".config/plugins/fm.lua".source              = src + "/files/plugins/fm.lua";
+    ".config/plugins/kanagawa.lua".source        = src + "/files/plugins/kanagawa.lua";
+    ".config/plugins/lsp-colors.lua".source      = src + "/files/plugins/lsp-colors.lua";
+    ".config/plugins/lualine.lua".source         = src + "/files/plugins/lualine.lua";
+    ".config/plugins/mason-lspconfig.lua".source = src + "/files/plugins/mason-lspconfig.lua";
+    ".config/plugins/modes.lua".source           = src + "/files/plugins/modes.lua";
+    ".config/plugins/neo-tree.lua".source        = src + "/files/plugins/neo-tree.lua";
+    ".config/plugins/noice.lua".source           = src + "/files/plugins/noice.lua";
+    ".config/plugins/notify.lua".source          = src + "/files/plugins/notify.lua";
+    ".config/plugins/scrollbar.lua".source       = src + "/files/plugins/scrollbar.lua";
+    ".config/plugins/sidebar.lua".source         = src + "/files/plugins/sidebar.lua";
+    ".config/plugins/telescope.lua".source       = src + "/files/plugins/telescope.lua";
+    ".config/plugins/toggleterm.lua".source      = src + "/files/plugins/toggleterm.lua";
+    ".config/plugins/which_key.lua".source       = src + "/files/plugins/which_key.lua";
+}
